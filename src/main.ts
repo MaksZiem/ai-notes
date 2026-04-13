16b730c75817d7de4a709c856103aec99720c5c4
MaksZiem <mtziembicki@gmail.com>
2026-04-13 22:47:14 +0200

    cors policy

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   (app as any).set('etag', false);
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
     next();
   });
+
   await app.listen(3000);
 }
 bootstrap();
